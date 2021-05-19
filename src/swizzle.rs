b6c3c2c726382707e553134cccfcea1c20fe3124
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2021-05-18 19:26:02 -0500

    improve swizzle tests for bc7

--- src/swizzle.rs
+++ src/swizzle.rs
@@ -39,23 +39,42 @@ pub fn swizzle_experimental<T: Copy>(
 }
 
 pub fn calculate_swizzle_pattern(width: u32, height: u32) -> (u32, u32) {
-    // TODO: This only works for powers of two.
-    // TODO: Use the code from the Tegra TRM?
-    
-    // Add the correct number of 1 bits based on the dimensions.
-    let y_shift = 5;
-    let y_mask = !0u32 >> (height.leading_zeros() + 6);
-    let y_pattern = 0b01101 | (y_mask << y_shift);
     let block_count = width * height / 16;
 
-    let y_pattern = y_pattern & (block_count - 1);
+    let y_pattern = swizzle_y(height / 4);
     let x_pattern = !y_pattern & (block_count - 1);
     (x_pattern, y_pattern)
 }
 
+fn swizzle_y(height_in_blocks: u32) -> u32 {
+    let y = !0 >> (height_in_blocks.leading_zeros() + 1);
+    (y & 0x1) | ((y & 0x6) << 1) | ((y & (!0 << 3)) << 2)
+}
+
 #[cfg(test)]
 mod tests {
+    use binread::{BinRead, BinReaderExt};
+
     use super::*;
+
+    fn read_blocks<T: BinRead>(bytes: &[u8]) -> Vec<T> {
+        let mut reader = std::io::Cursor::new(bytes);
+        let mut blocks = Vec::new();
+        while let Ok(block) = reader.read_le::<T>() {
+            blocks.push(block);
+        }
+        blocks
+    }
+
+    #[test]
+    fn swizzle_y_bc7() {
+        // This takes the width in blocks as input.
+        assert_eq!(0b101101, swizzle_y(64 / 4));
+        assert_eq!(0b1101101, swizzle_y(128 / 4));
+        assert_eq!(0b11101101, swizzle_y(256 / 4));
+        assert_eq!(0b111101101, swizzle_y(512 / 4));
+
+    }
 
     #[test]
     fn bit_pattern_bc7_64_64() {
@@ -87,5 +106,48 @@ mod tests {
             (0b11111000010010, 0b00000111101101),
             calculate_swizzle_pattern(512, 512)
         );
+    }
+
+    
+    #[test]
+    fn deswizzle_bc7_128_128() {
+        let input: Vec<u128> = read_blocks(include_bytes!("../swizzle_data/128_bc7_linear.bin"));
+        let expected: Vec<u128> = read_blocks(include_bytes!("../swizzle_data/128_bc7_linear_deswizzle.bin"));
+        let mut actual = vec![0u128; 128 * 128 / 16];
+
+        let (x_mask, y_mask) = calculate_swizzle_pattern(128, 128);
+        let x_mask = x_mask as i32;
+        let y_mask = y_mask as i32;
+        swizzle_experimental(x_mask, y_mask, 128 / 4, 128 / 4, &input, &mut actual, true);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn deswizzle_bc7_256_256() {
+        let input: Vec<u128> = read_blocks(include_bytes!("../swizzle_data/256_bc7_linear.bin"));
+        let expected: Vec<u128> = read_blocks(include_bytes!("../swizzle_data/256_bc7_linear_deswizzle.bin"));
+        let mut actual = vec![0u128; 256 * 256 / 16];
+
+        let (x_mask, y_mask) = calculate_swizzle_pattern(256, 256);
+        let x_mask = x_mask as i32;
+        let y_mask = y_mask as i32;
+        swizzle_experimental(x_mask, y_mask, 256 / 4, 256 / 4, &input, &mut actual, true);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn deswizzle_bc7_512_512() {
+        let input: Vec<u128> = read_blocks(include_bytes!("../swizzle_data/512_bc7_linear.bin"));
+        let expected: Vec<u128> = read_blocks(include_bytes!("../swizzle_data/512_bc7_linear_deswizzle.bin"));
+        let mut actual = vec![0u128; 512 * 512 / 16];
+
+        let (x_mask, y_mask) = calculate_swizzle_pattern(512, 512);
+        let x_mask = x_mask as i32;
+        let y_mask = y_mask as i32;
+        swizzle_experimental(x_mask, y_mask, 512 / 4, 512 / 4, &input, &mut actual, true);
+
+        assert_eq!(expected, actual);
     }
 }
