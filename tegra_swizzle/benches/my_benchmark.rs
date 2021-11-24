535deadbf8e58f139a79aff33cad2c1cc75122d5
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2021-11-23 22:00:59 -0600

    add swizzle benchmark

--- tegra_swizzle/benches/my_benchmark.rs
+++ tegra_swizzle/benches/my_benchmark.rs
@@ -1,6 +1,6 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-use tegra_swizzle::{block_height_mip0, div_round_up};
+use tegra_swizzle::{BlockHeight, block_height_mip0, div_round_up, mip_block_height};
 
 pub fn div_round_up_benchmark(c: &mut Criterion) {
     c.bench_function("div_round_up", |b| b.iter(|| div_round_up(black_box(10), 4)));
@@ -11,5 +11,9 @@ pub fn block_height_mip0_benchmark(c: &mut Criterion) {
     c.bench_function("block_height_mip0", |b| b.iter(|| block_height_mip0(black_box(512))));
 }
 
-criterion_group!(benches, div_round_up_benchmark, block_height_mip0_benchmark);
+pub fn mip_block_height_benchmark(c: &mut Criterion) {
+    c.bench_function("mip_block_height", |b| b.iter(|| mip_block_height(black_box(512), black_box(BlockHeight::Sixteen))));
+}
+
+criterion_group!(benches, div_round_up_benchmark, block_height_mip0_benchmark, mip_block_height_benchmark);
 criterion_main!(benches);
---
+++ tegra_swizzle/benches/swizzle.rs
@@ -0,0 +1,27 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use tegra_swizzle::BlockHeight;
+use tegra_swizzle::swizzle_block_linear;
+use tegra_swizzle::swizzled_surface_size;
+
+use criterion::BenchmarkId;
+use criterion::Throughput;
+
+fn swizzle_block_linear_benchmark(c: &mut Criterion) {
+    let block_height = BlockHeight::Sixteen;
+    let bytes_per_pixel = 4;
+    // We'll allocated the size needed by the largest run.
+    // This avoids including the allocation time in the benchmark.
+    let source = vec![0u8; swizzled_surface_size(512, 512, 1, block_height, bytes_per_pixel)];
+
+    let mut group = c.benchmark_group("swizzle_block_linear");
+    for size in [0, 32, 64, 128, 256, 320, 340, 384, 448, 464, 500, 512] {
+        group.throughput(Throughput::Bytes((size * size * bytes_per_pixel) as u64));
+        group.bench_with_input(BenchmarkId::from_parameter(size), &size, |b, &size| {
+            b.iter(|| swizzle_block_linear(size, size, 1, &source, block_height, bytes_per_pixel));
+        });
+    }
+    group.finish();
+}
+
+criterion_group!(benches, swizzle_block_linear_benchmark);
+criterion_main!(benches);
